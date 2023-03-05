c6bdf520ffdf3b822da22e01408f790f75f8efb2
Yujiang Bi <byujiang@gmail.com>
2023-03-05 09:19:12 +0800

    update

--- do.sh
+++ do.sh
@@ -9,7 +9,7 @@ dotfile_path=$(dirname `readlink -f $0`)
 if [[ -f $HOME/.zshrc ]]; then
 	grep -q "source ${dotfile_path}/shell/shell.sh" ~/.zshrc || echo "source ${dotfile_path}/shell/shell.sh" >> ~/.zshrc
 	#echo 'ZSH_THEME="agnoster"' >> ~/.zshrc
-	sed -i "s|robbyrussell|fino-time|g"  ~/.zshrc
+	sed -i "s|robbyrussell|fino-time|g"  $HOME/.zshrc
 	if [[ -d $HOME/.oh-my-zsh/themes ]]; then
 		cp ${dotfile_path}/shell/fino-time.zsh-theme ~/.oh-my-zsh/themes
 	fi
--- shell/comm.sh
+++ shell/comm.sh
@@ -3,9 +3,9 @@
 
 #### system part
 export EDITOR=vim
-#export LANG=en_US.UTF-8
-#export LC_ALL=en_US.UTF-8
-#export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
 # export XMODIFIERS=@im=fcitx
 
 #### clash
